8b91ca5c368461fe44e9bb45f21e34d96f1550fd
nardis0321 <nardis0321@gmail.com>
2023-01-18 09:45:17 +0900

    vo 틀 작성

---
+++ bbodeum/src/main/java/com/bbodeum/trainer/vo/Trainer.java
@@ -0,0 +1,10 @@
+package com.bbodeum.trainer.vo;
+
+public class Trainer {
+	private String trId;
+	private String trPwd;
+	private String trName;
+	private String trTel;
+	private String trEmail;
+	
+}
